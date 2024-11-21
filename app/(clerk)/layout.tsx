31a36dc73d528e61e2d6a3c12beeea049667be10
omadbek <abdullayevomadbek709@gmail.com>
2024-11-21 19:49:35 +0500

    Video-Call project boshlandi. Auth uchun clerck orqali authdan foydalandim

---
+++ app/(clerk)/layout.tsx
@@ -0,0 +1,8 @@
+
+const ClerkLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <section className=" h-screen flex items-center justify-center">{children}</section>
+  )
+}
+
+export default ClerkLayout
---
+++ components/layout/Container.tsx
@@ -0,0 +1,5 @@
+const Container = ({ children }: { children: React.ReactNode }) => {
+  return <section className=" max-w-[1920px] w-full mx-auto xl:pz-20  px-4 py-4">{children}</section>;
+};
+
+export default Container;
